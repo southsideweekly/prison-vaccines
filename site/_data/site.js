6728e0c22a8ca55a20a0636db1a8ed3a1e278e52
pjsier <pjsier@gmail.com>
2021-03-08 20:21:51 -0600

    refactor: update copy, metadata

--- site/_data/site.js
+++ site/_data/site.js
@@ -4,13 +4,11 @@ const host = production ? process.env.SITE_HOST : "http://localhost:8080"
 
 const baseurl = production ? "/prison-vaccines" : ""
 
-// TODO: Update metadata
 module.exports = {
   name: "South Side Weekly",
-  title:
-    "Inmates at Stateville Correctional Center describe COVID-19 behind bars: ‘Everything that was done felt like a punishment’ | South Side Weekly",
+  title: "Everything Felt Like a Punishment | South Side Weekly",
   description:
-    "“I fear more of us will perish due to incompetence, neglect and apathy,” said Williams La’Roy Peeples Jr.",
+    "Inmates at Stateville Correctional Center describe COVID-19 behind bars",
   type: "article",
   publisher: "http://facebook.com/southsideweekly",
   facebookPageId: "622453664466624",
